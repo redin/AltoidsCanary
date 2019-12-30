9c33a5d2513e52a2bf0bf899f6cf970cd888f018
redin <ricardo.redin@gmail.com>
2019-12-30 13:43:16 -0300

    Adding buzzer and matching PCB design pins

--- AltoidsCanary.ino
+++ AltoidsCanary.ino
@@ -5,29 +5,37 @@
 #define HT 60
 #define RT 90
 
-
 TM1637Display display(CLK, DIO);
 
 int coPin = A0;  //Carbon Monoxide
 int foPin = A1;  //Flamable and Smoke
 int ledPin = 13;
 int pin = 9;
-int pin2 = 10;
+int buzzerPin = 10;
 int coValue = -999;
 int foValue = -998;
+int coMean = 0;
+int foMean = 0;
 int h = 0;
 int r = 0;
 int st = 0;
 
+void beep(int delayms) {
+  analogWrite(buzzerPin, 0);
+  delay(delayms);
+  analogWrite(buzzerPin, 255); 
+}
 
 void setup() {
   analogReference(EXTERNAL);
   pinMode(ledPin, OUTPUT);
   pinMode(pin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
   pinMode(coPin, INPUT);
   pinMode(foPin, INPUT);
   Serial.begin(9600);
   display.setBrightness(0x0f);
+  beep(50);
 }
 
 void loop() {
@@ -36,29 +44,40 @@ void loop() {
     Serial.println(h);
     analogWrite(ledPin, 255);
     analogWrite(pin, 255);
-    analogWrite(pin2, 255);
     h++;  
   }else if (h >= HT && r < RT){
     Serial.print("Reading 90s...");
     Serial.println(r);
     analogWrite(ledPin, 79);
     analogWrite(pin, 79);
-    analogWrite(pin2, 79);
     r++;
   }else if (h >= HT && r >= RT){
-    Serial.println("Collecting 150ms...");
+    Serial.println("Collecting 100ms...");
     analogWrite(pin, 255);
-    analogWrite(pin2, 255);
     delay(100);
-    coValue = map(analogRead(coPin), 10, 1023, 10, 10000);
-    foValue = map(analogRead(foPin), 10, 1023, 300, 10000);
+    coValue = map(analogRead(coPin), 0, 1023, 10, 10000);
+    foValue = map(analogRead(foPin), 0, 1023, 300, 10000);
+    if(coMean == foMean){
+      coMean = coValue;
+      foMean = foValue;
+    }
+    coMean = (coMean + coValue)/2;
+    foMean = (foMean + foValue)/2;
     r = 0;
-    h = 0;  
+    h = 0;
+    beep(10);
   }
   Serial.print("CO = ");
   Serial.print(coValue);
   Serial.print(" FO = ");
   Serial.println(foValue);
+  if((coValue - coMean) >  (coMean /2) ){
+    beep(500); 
+  }
+  if((foValue - foMean) >  (foMean /2) ){
+    beep(500);
+  }
+  
   if(((h+r)%5) == 0){
     if(st == 0){
       display.showNumberDec(coValue, false);
